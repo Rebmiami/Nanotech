3f126edcfa9ff4744ee198401eb3adbbcb752bc3
Rebmiami <59275598+Rebmiami@users.noreply.github.com>
2022-10-27 08:45:22 -0400

    Added some utilities and updated properties

--- Nanotech.lua
+++ Nanotech.lua
@@ -13,6 +13,28 @@ if not nanotech then
     nanotech = {}
 end
 
+-- Tables and utility functions
+local photDataTypes = {
+    [elem.DEFAULT_PT_FILT] = true,
+    [elem.DEFAULT_PT_PHOT] = true,
+    [elem.DEFAULT_PT_BRAY] = true,
+}
+
+local usnsNoConduct = {
+    [elem.DEFAULT_PT_WATR] = true,
+    [elem.DEFAULT_PT_SLTW] = true,
+    [elem.DEFAULT_PT_NTCT] = true,
+    [elem.DEFAULT_PT_PTCT] = true,
+    [elem.DEFAULT_PT_INWR] = true,
+}
+
+local function acceptedConductor(r)
+    local type = sim.partProperty(r, "type")
+    return bit.band(elem.property(type, "Properties"), elem.PROP_CONDUCTS) and not usnsNoConduct(type) and sim.partProperty(r, "life") == 0
+end
+
+
+
 -- Element definitions
 local nano = elem.allocate("NANOTECH", "NANO") -- Nanobots
 local snano = elem.allocate("NANOTECH", "SNANO") -- Solid nanobots
@@ -25,28 +47,49 @@ local usns = elem.allocate("NANOTECH", "USNS") -- Universal sensor
 -- NANO
 elem.element(nano, elem.element(elem.DEFAULT_PT_EQVE))
 elem.property(nano, "Name", "NANO")
-elem.property(nano, "Description", "Nanobots. Programmable critters with several modes and customizable modifiers.")
+elem.property(nano, "Description", "Nanobots. Programmable critters with several modes and flexible modifiers. Shift-click to program.")
 elem.property(nano, "Colour", 0x0B2D4B)
-elem.property(nano, "HighTemperature", 9000)
+elem.property(nano, "HighTemperature", 6000)
 elem.property(nano, "HighTemperatureTransition", elem.DEFAULT_PT_BREL)
+elem.property(nano, "MenuVisible", 1)
 elem.property(nano, "MenuSection", elem.SC_POWERED)
+elem.property(nano, "Diffusion", 0.1)
+elem.property(nano, "HeatConduct", 5)
 
 elem.property(nano, "Update", function(i, x, y, s, n)
-    nanobotUpdate(i, x, y)
+    nanoUpdate(i, x, y)
 end)
 
 -- USNS
 elem.element(usns, elem.element(elem.DEFAULT_PT_DTEC))
 elem.property(usns, "Name", "USNS")
-elem.property(usns, "Description", "Universal sensor. Highly configurable sensor with serialization capabilities.")
+elem.property(usns, "Description", "Universal sensor. Highly configurable sensor with serialization capabilities. Shift-click to configure.")
 elem.property(usns, "Colour", 0x6920CF)
 
 elem.property(usns, "Update", function(i, x, y, s, n)
-    
+    usnsUpdate(i, x, y)
 end)
 
+elem.property(usns, "CtypeDraw", function(i, t)
+	if bit.band( elem.property(t, "Properties"), elem.PROP_NOCTYPEDRAW) == 0 then
+		sim.partProperty(i, "ctype", t)
+	end
+end)
 
-
-function nanobotUpdate(i, x, y)
+-- NANO logic
+function nanoUpdate(i, x, y)
 
 end
+
+-- USNS logic
+function usnsUpdate(i, x, y)
+
+end
+
+
+-- Particle editing logic
+-- TODO: Detect if the user shift-clicked a particle of NANO or USNS
+
+-- TODO: NANO configuration UI
+
+-- TODO: USNS configuration UI
